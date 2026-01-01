eb0ef982dfdd7c1c60205736a9d861f435fb4177
inaba1115 <inaba1115@gmail.com>
2026-01-02 01:14:08 +0900

    dev

--- src/hyperscore/rhythm_tree.py
+++ src/hyperscore/rhythm_tree.py
@@ -6,60 +6,47 @@ from lark import Lark, Transformer
 
 
 @dataclass(frozen=True)
-class RhythmTree:
-    nodes: list
+class RhythmNode:
+    weight: float
 
     def expand(self, ms: int) -> list[int]:
-        ms_sum = ms
-        value_sum = 0
-        for n in self.nodes:
-            value_sum += n.value
-
-        ret = []
-        for i in range(len(self.nodes) - 1):
-            node = self.nodes[i]
-            assignment = int(ms * node.value / value_sum)
-            ret.extend(node.expand(assignment))
-            ms_sum -= assignment
-
-        node = self.nodes[-1]
-        assignment = ms_sum
-        ret.extend(node.expand(assignment))
-
-        return ret
+        raise NotImplementedError
 
 
 @dataclass(frozen=True)
-class Atom:
-    value: int
-
+class Atom(RhythmNode):
     def expand(self, ms: int) -> list[int]:
         return [ms]
 
 
 @dataclass(frozen=True)
-class Children:
-    value: int
-    nodes: RhythmTree
-
-    def expand(self, ms: int) -> list[int]:
-        return self.nodes.expand(ms)
-
-
-@dataclass(frozen=True)
-class Division:
-    value: int
+class Division(RhythmNode):
     denominator: int
 
     def expand(self, ms: int) -> list[int]:
         unit = int(ms / self.denominator)
+        ret = [unit] * (self.denominator - 1)
+        ret.append(ms - unit * (self.denominator - 1))
+        return ret
+
+
+@dataclass(frozen=True)
+class RhythmTree(RhythmNode):
+    children: list[RhythmNode]
+
+    def expand(self, ms: int) -> list[int]:
+        # TODO: use largest remainder method and fraction
+
+        ms_tmp = ms
+        weight_sum = sum([n.weight for n in self.children])
 
         ret = []
-        for i in range(self.denominator - 1):
-            ret.append(unit)
+        for n in self.children[:-1]:
+            assign_ms = int(ms * n.weight / weight_sum)
+            ret.extend(n.expand(assign_ms))
+            ms_tmp -= assign_ms
 
-        ret.append(ms - unit * (self.denominator - 1))
-
+        ret.extend(self.children[-1].expand(ms_tmp))
         return ret
 
 
@@ -71,7 +58,7 @@ class RhythmTreeTransformer(Transformer):
                 ret.extend(t)
             else:
                 ret.append(t)
-        return RhythmTree(ret)
+        return RhythmTree(1, ret)
 
     def repeat(self, tree: list):
         ret = []
@@ -83,7 +70,7 @@ class RhythmTreeTransformer(Transformer):
         return Atom(int(tree[0]))
 
     def children(self, tree: list):
-        return Children(int(tree[0]), tree[1])
+        return RhythmTree(int(tree[0]), tree[1])
 
     def division(self, tree: list):
         return Division(int(tree[0]), int(tree[1]))

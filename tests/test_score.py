b222b9881af6e9d41b29c18877b9a41e657634af
inaba1115 <inaba1115@gmail.com>
2026-01-02 03:19:43 +0900

    dev

---
+++ tests/test_score.py
@@ -0,0 +1,27 @@
+import unittest
+
+import hyperscore
+
+
+class TestZippedNotes(unittest.TestCase):
+    def test_iter_events(self):
+        source = hyperscore.ZippedNotes(
+            pitch=[60, 62], velocity=[80, 100], duration=[100, 200], gate=[0.5], probability=[0.5], channel=[0, 1]
+        )
+        context = hyperscore.ScoreContext(cursor_ms=500)
+        events = list(source.iter_events(context))
+        self.assertEqual(len(events), 2)
+        self.assertEqual(events[0].pitch, 60)
+        self.assertEqual(events[0].velocity, 80)
+        self.assertEqual(events[0].start_ms, 500)
+        self.assertEqual(events[0].duration_ms, 100)
+        self.assertEqual(events[0].gate, 0.5)
+        self.assertEqual(events[0].probability, 0.5)
+        self.assertEqual(events[0].channel, 0)
+        self.assertEqual(events[1].pitch, 62)
+        self.assertEqual(events[1].velocity, 100)
+        self.assertEqual(events[1].start_ms, 600)
+        self.assertEqual(events[1].duration_ms, 200)
+        self.assertEqual(events[1].gate, 0.5)
+        self.assertEqual(events[1].probability, 0.5)
+        self.assertEqual(events[1].channel, 1)

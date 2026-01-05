6571d95fbbf541a362350d0b1047babf7e3a1d4d
inaba1115 <inaba1115@gmail.com>
2026-01-06 03:56:12 +0900

    dev

--- examples/on_the_run.py
+++ examples/on_the_run.py
@@ -1,19 +1,19 @@
 import mido
 
-import hyperscore
+import hyperscore as hs
 
-score = hyperscore.Score()
+score = hs.Score()
 
 arp = [52, 55, 57, 55, 62, 60, 62, 64]
 
-ast = hyperscore.parse_rhythm("1*16")
-duration = hyperscore.rhythm_ast_to_ticks(ast, total_ticks=int(hyperscore.bpm_to_ms(165, 4)))
+ast = hs.parse_rhythm("1*16")
+duration = hs.rhythm_ast_to_ticks(ast, total_ticks=int(hs.bpm_to_ms(165, 4)))
 
 for _ in range(8):
-    score.add(pitch=arp, duration=duration, event_factory=hyperscore.NoteEvent)
+    score.add(pitch=arp, duration=duration, event_factory=hs.NoteEvent)
 
-# exporter = hyperscore.MidiExporter()
+# exporter = hs.MidiExporter()
 # exporter.export(score, "foo.mid")
 
-player = hyperscore.MidiPlayer(output=mido.get_output_names()[0])  # type: ignore
+player = hs.MidiPlayer(output=mido.get_output_names()[0])  # type: ignore
 player.play(score)

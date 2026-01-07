b385db8decb5961eedd4a3a26f6e23c5492bcfca
inaba1115 <inaba1115@gmail.com>
2026-01-07 20:52:29 +0900

    dev

---
+++ examples/minimal.py
@@ -0,0 +1,39 @@
+from hyperscore import CHORDS, Score, parse_rhythm
+from hyperscore.core import NoteEvent, bpm_to_ms
+from hyperscore.io import MidiExporter
+from hyperscore.rhythm import rhythm_ast_to_timespans
+
+# ----------------
+# theory
+# ----------------
+chord = CHORDS["major7"]
+
+pitches = [n for n in range(60, 72) if n % 12 in chord.intervals]
+pitch_iter = iter(pitches)
+
+# ----------------
+# rhythm
+# ----------------
+ast = parse_rhythm("1*4")
+total = int(bpm_to_ms(120, 1))
+spans = rhythm_ast_to_timespans(ast, total=total)
+
+# ----------------
+# score
+# ----------------
+score = Score()
+
+score.add_timespans(
+    spans,
+    factory=lambda span: NoteEvent(
+        pitch=next(pitch_iter),
+        velocity=100,
+        span=span,
+        channel=0,
+    ),
+)
+
+# ----------------
+# output
+# ----------------
+MidiExporter().export(score, "example.mid")
